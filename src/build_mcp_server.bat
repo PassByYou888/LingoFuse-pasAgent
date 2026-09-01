b3c7805bf34475b30f53b42de244566fe51c01c9
老张 <passbyyou888@outlook.com>
2026-09-01 14:19:48 +0800

    Create build_mcp_server.bat

---
+++ src/build_mcp_server.bat
@@ -0,0 +1,1 @@
+pyinstaller --onefile --collect-all fastmcp --collect-all pydantic --collect-all tzdata --hidden-import language_middleware --hidden-import lingofuse --paths . --add-data "lingofuse;lingofuse" mcp_server.py
